ccb9765b4df95bcb683cb1347c0b0c5e4a1d69a8
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-10-22 19:11:25 -0600

    ADD draw player and ball with new dimentions

--- game.py
+++ game.py
@@ -5,6 +5,8 @@
 # Donaldo Garcia
 from constants import *
 from soccer import Soccer
+import numpy as np
+import skfuzzy as fuzz
 import pygame
 
 pygame.init()
@@ -13,13 +15,18 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('LOGICA DIFUSA')
 # pygame.display.set_icon(pygame.image.load('icon.jpg'))
 
+background = pygame.image.load('images/field.jpg').convert_alpha()
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 g = Soccer(screen)
-background = pygame.image.load('images/field.jpg').convert()
 
+x_mov = np.arange(0, HEIGHT, 1)
+y_mov = np.arange(0, WIDTH, 1)
 
 game_on = True
 while game_on:
 	screen.blit(background, (0, 0))
+	g.draw_player(10, 200)
+	g.draw_ball(200, 200)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			game_on = False
--- soccer.py
+++ soccer.py
@@ -5,5 +5,24 @@ from constants import *
 class Soccer(object):
 	def __init__(self, screen):
 		self.screen = screen
-		
+		self.draw_player(10, 200)
+
+	def clear(self):
+		self.screen.fill((0, 0, 0))
+
+	def pixel(self, x, y, color=WHITE):
+		self.screen.set_at((x, y), color)
 	
+	def big_pixel(self, x, y, color=WHITE):
+		# draw a pixel, of size 5x5 pixels with center at (x, y)
+		for i, j in itertools.product(range(10), range(10)):
+			self.pixel(x+i, y+j, color)
+
+	def copy(self):
+		self.last_screen = self.screen.copy()
+	
+	def draw_player(self, x, y, color=RED):
+		self.big_pixel(x, y, color)
+
+	def draw_ball(self, x, y, color=BLACK):
+		self.big_pixel(x, y, color)
